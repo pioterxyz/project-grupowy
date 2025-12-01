35358bd9750d443e18af17b9387c8ac8dcc0260c
estezet <curokyt@gmail.com>
2025-12-01 08:25:25 +0100

    Implement DataContext for managing Pokemon data

---
+++ DataContext.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { fetchPokemonList } from "../services/api";
+
+const DataContext = createContext();
+export const useData = () => useContext(DataContext);
+
+export default function DataProvider({ children }) {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchPokemonList().then((data) => {
+      setItems(data.results);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <DataContext.Provider value={{ items, loading }}>
+      {children}
+    </DataContext.Provider>
+  );
+}
+
+
+index jsx
+import DataProvider from "./context/DataContext";
+
+root.render(
+  <DataProvider>
+    <App />
+  </DataProvider>
+);
