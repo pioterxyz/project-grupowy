8d2113373f1c15e0527894c848c3728f8f988bd2
estezet <curokyt@gmail.com>
2025-12-01 08:26:40 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:name" element={<Details />} />
+          <Route path="*" element={<h1 className="text-3xl">404 - Not Found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
