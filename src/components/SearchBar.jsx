09d8eebc22a76887b9b4d3bbd7364c47867a59b0
estezet <curokyt@gmail.com>
2025-12-01 08:45:07 +0100

    + searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+export default function SearchBar({ value, onChange }) {
+    return (
+        <input
+            type="text"
+            placeholder="Search..."
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            className="w-full p-2 border rounded-md shadow mb-4"
+        />
+    );
+}
