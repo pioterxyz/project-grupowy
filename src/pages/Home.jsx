4e1d4ad9606daa9afdff5714057bf857b8864883
estezet <curokyt@gmail.com>
2025-12-01 11:00:27 +0100

    + pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,19 @@ export default function Home() {
   const currentItems = filtered.slice(indexOfFirstItem, indexOfLastItem);
 
   const totalPages = Math.ceil(filtered.length / itemsPerPage);
+
+  function getVisiblePages(currentPage, totalPages) {
+    const pages = [];
+    pages.push(1);
+    if (currentPage > 3) pages.push("...");
+    for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+      if (i > 1 && i < totalPages) pages.push(i);
+    }
+    if (currentPage < totalPages - 2) pages.push("...");
+    if (totalPages > 1) pages.push(totalPages);
+    return pages;
+  }
+
   return (
     <div>
       <SearchBar value={search} onChange={setSearch} />
