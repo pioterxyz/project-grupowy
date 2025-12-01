3f4d40fc1e5f40f993bfd951d75c1a1c3d55734e
estezet <curokyt@gmail.com>
2025-12-01 08:43:22 +0100

    + navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-600 text-white px-6 py-3 text-xl font-semibold">
+            Simple Data Explorer
+        </nav>
+    );
+}
